bff5999c3f4800571288a36535a951b119b7cc13
Sara <saraenge@kth.se>
2019-01-23 11:47:02 +0100

    guest change working in both mobile desktop

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -3,6 +3,6 @@ var SidebarView = function (container, model) {
 
 	model.setNumberOfGuests(5);
 
-	document.getElementById('numPeople').value = model.numberOfGuests;
-	
+	document.getElementById('numPeopleSide').value = model.numberOfGuests;
+	document.getElementById('numPeopleNav').value = model.numberOfGuests;	
 }
