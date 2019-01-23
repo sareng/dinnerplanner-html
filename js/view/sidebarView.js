07bb8613d056b960d77b041085c9ba34b283485d
Sara <saraenge@kth.se>
2019-01-23 11:29:55 +0100

    sidebar view changing the nunmber of guests on select dish

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -4,11 +4,12 @@ var SidebarView = function (container, model) {
 	var guests = container.find("#numPeople");
 
 	model.setNumberOfGuests(5);
-	// console.log("num guests model: " + model.numberOfGuests);
-	// console.log("number input object: " + guests);
+	console.log("num guests model: " + model.numberOfGuests);
+	console.log("number input object: " + guests);
 	guests.value = model.numberOfGuests;
 
 	document.getElementById('numPeople').value = model.numberOfGuests;
+	console.log("numpeople value: " + document.getElementById('numPeople').value);
 
 	// model.setNumberOfGuests(11);	
 	// document.getElementById('numPeople').value = model.numberOfGuests;
