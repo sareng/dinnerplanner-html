ca7c2d19bb14739ee4ce515a4b9786bd4b5724e3
Sara <saraenge@kth.se>
2019-01-23 11:31:56 +0100

    sidebar view less mess

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -1,20 +1,8 @@
 
 var SidebarView = function (container, model) {
-	
-	var guests = container.find("#numPeople");
 
 	model.setNumberOfGuests(5);
-	console.log("num guests model: " + model.numberOfGuests);
-	console.log("number input object: " + guests);
-	guests.value = model.numberOfGuests;
 
 	document.getElementById('numPeople').value = model.numberOfGuests;
-	console.log("numpeople value: " + document.getElementById('numPeople').value);
-
-	// model.setNumberOfGuests(11);	
-	// document.getElementById('numPeople').value = model.numberOfGuests;
-
-
-	// document.getElementById('gadget_url').value = '';
 	
 }
