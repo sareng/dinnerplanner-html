cc4bf62ca0dc950cdaee3567ea0ec44026fb3ac3
Sara <saraenge@kth.se>
2019-01-29 13:41:55 +0100

    new views started

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@ $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
 	
-	// And create the instance of ExampleView
-	// var exampleView = new ExampleView($("#exampleView"), model);
+	var startView = new StartView($("#startView"), model);
 	var sidebarView = new SidebarView($("#sidePanel"), model);
 	var dishView = new DishView($("#dishView"), model);
 	var menuView = new MenuView($("#dishCard"), model);
+	var dinnerPrintView = new DinnerPrintView($("#dinnerPrintView"), model);
+	var dinnerSummaryView = new DinnerSummaryView($("#dinnerSummaryView"), model);
+
 
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
---
+++ js/view/dinnerPrintView.js
@@ -0,0 +1,13 @@
+
+var DinnerPrintView = function (container, model) {
+
+	content = 	
+		"<div class=\"row \">" + "<div class=\"col-3\"></div>"+ "<div class=\"col-6 index\">"+
+				"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mattis fermentum lectus et maximus. Sed gravida dignissim quam, consectetur facilisis nisi faucibus quis. Nunc non purus pharetra, suscipit sem eu, hendrerit magna. Integer in pellentesque lacus. Nulla lectus ante, lobortis at posuere at, elementum vel quam. Integer eu gravida leo. hasellus vitae ex non elit ultricies scelerisque a ut dui. Mauris auctor vulputate placerat.</p>" +
+				"<br />" + 
+				"<button type=\"button\" class=\"btn\">Create New Dinner</button>" +
+			"</div>" + 
+		"</div>";
+
+	container.html(content);
+}
