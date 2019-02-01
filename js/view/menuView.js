04760718bd6fff530fc8942fede97dbafdcbe8fc
Sara <saraenge@kth.se>
2019-02-01 01:24:40 +0100

    give pictures in select dish view ids from id of dish

--- js/view/menuView.js
+++ js/view/menuView.js
@@ -6,7 +6,7 @@ var  MenuView = function (container, model) {
 	var card = [];
 	allDishes.forEach(dish => {
 		card.push("<div class=\"col-xs-12 col-sm-12 col-md-3 col-lg-3\">",
-			"<div class=\"card text-center\">",
+			"<div class=\"card text-center\" id=\"dish", dish.id, "\">",
 			"<img id=\"dishImage\" class=\"card-img-top\" src=\"images/",dish.image,"\" alt=\"",dish.name,"\">",
 			"<div class=\"card-body\">",
 			"<h5 class=\"card-title\" id=\"dishTitle\">",dish.name,"</h5>",
